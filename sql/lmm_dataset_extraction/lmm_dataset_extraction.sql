8167a770eb073ecbc62ecf127e9fbcd58e847054
Olivier Brown <olivierjeanbrown@gmail.com>
2025-11-29 16:17:00 -0500

    update sql

--- sql/lmm_dataset_extraction/lmm_dataset_extraction.sql
+++ sql/lmm_dataset_extraction/lmm_dataset_extraction.sql
@@ -1,11 +1,23 @@
 -- only select variables of interest for interaction LMM model
-SELECT record_id, cdrisc_72, cdrisc_4w, sex, calc_age, randomization FROM MBI4mTBIoutput
+SELECT 
+    m.record_id, 
+    m.cdrisc_72, 
+    m.cdrisc_4w, 
+    m.sex, 
+    m.calc_age, 
+    m.randomization,
+    p.*  -- include all columns from PartnerReport
+FROM MBI4mTBIoutput AS m
+-- join PartnerReport data based on record_id
+LEFT JOIN PartnerReport AS p
+    ON p.sid = m.record_id
 -- only include those that completed BOTH MRIs
-where mri_comp = 1 AND mri_comp_2 = 1
+WHERE m.mri_comp = 1 
+  AND m.mri_comp_2 = 1
 -- only include those that adhered to the protocol (spent 9600 in app activities)
-	AND session_duration_sum > 9600 
+  AND m.session_duration_sum > 9600 
 -- only include those with existing values for relevant variables (cdrisc)
-	AND cdrisc_72 IS NOT NULL 
-	AND cdrisc_4w IS NOT NULL
+  AND m.cdrisc_72 IS NOT NULL 
+  AND m.cdrisc_4w IS NOT NULL
 -- sort by randomization, specifically grouping MBIs first then Shams
-ORDER BY randomization ASC;
+ORDER BY m.randomization ASC;
